2b66de402a90ed88f411d2a52ec07ddd0c6f8fc0
Yianni Ziouras <jmziouras@gmail.com>
2025-08-31 16:49:47 -0700

    recreated structure

--- grocery_tracker/blog.py
+++ grocery_tracker/blog.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, url_for
 from werkzeug.exceptions import abort
 
-from flaskr.auth import login_required
-from flaskr.db import get_db
+from grocery_tracker.auth import login_required
+from grocery_tracker.db import get_db
 
 bp = Blueprint("blog", __name__)
 
